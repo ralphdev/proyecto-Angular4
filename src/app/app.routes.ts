6ad2dd27812ba33cebffcf0885021cb3446e180b
ralphdev <rafa.developers@gmail.com>
2017-09-24 21:58:27 -0500

    Modificacion app.routes.ts para lo del link class active

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,10 @@ import {
 } from "./components/index.paginas";
 
 const app_routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'productos', component: ProductosComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});

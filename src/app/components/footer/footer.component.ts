c9835ee341e41a00df6dd6cda25770205a6bcaca
ralphdev <rafa.developers@gmail.com>
2017-10-06 23:39:52 -0500

    Servicios header y footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InformacionService } from "../../services/informacion.services";
 
 @Component({
   selector: 'app-footer',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class FooterComponent {
 
   anio:number = new Date().getFullYear();
+
+  constructor( public _is:InformacionService ){  }
 }

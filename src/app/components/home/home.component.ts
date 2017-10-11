52eded94c982151d52edcede283681b8befc2636
ralphdev <rafa.developers@gmail.com>
2017-10-11 01:25:44 -0500

    Agrege Firebase en Servicios Productos

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  
+
+  constructor( private _ps:ProductosService ){
+    
+  }
+
 }
